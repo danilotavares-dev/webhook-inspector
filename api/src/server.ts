9a7de59bab329c608907375fbd86f53a4f4d076d
danilotavares-dev <danilotavarespr00@gmail.com>
2025-11-02 17:31:52 -0300

    feat: create api structure with docker, drizzle and fastify

---
+++ api/src/server.ts
@@ -0,0 +1,45 @@
+import fastify from 'fastify'
+import {
+  validatorCompiler,
+  serializerCompiler,
+  jsonSchemaTransform,
+  type ZodTypeProvider
+} from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifyCors } from '@fastify/cors'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+import { listWebhooks } from './routes/list-webhooks'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  // credentions: true,
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Webhook Inspector API',
+      description: 'API for capturing and inspecting webhook requests',
+      version: '1.0.0'
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(ScalarApiReference, {
+  routePrefix: '/docs',
+})
+
+app.register(listWebhooks)
+
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log('HTTP server running!')
+  console.log('HTTP server runing on http://localhost:3333')
+  console.log('Docs available at http://localhost:3333/docs')
+})
