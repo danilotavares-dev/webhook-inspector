00636e4aedb09ac9eb01527d3f3e387fc59aa2ce
danilotavares-dev <danilotavarespr00@gmail.com>
2026-05-26 16:46:05 -0300

    fix: railway deploy config

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
     url: env.DATABASE_URL,
   },
   out: './src/db/migrations',
-  schema: './src/db/schema/index.ts',
+  schema: './src/db/schema/index.ts ',
   casing: 'snake_case',
 })
